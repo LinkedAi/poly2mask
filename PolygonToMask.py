6355f9369970628cc87ea8865ba13e12b806cc3b
Diego Parra <diego@linkedai.co>
2020-04-12 23:04:42 -0400

    Add demo images

--- PolygonToMask.py
+++ PolygonToMask.py
@@ -16,23 +16,33 @@ def createMasks(image, data):
   for elem in data:
     mask = np.zeros(image.shape[0:2], dtype=np.uint8)
     arr = np.zeros((len(elem['pos']), 2))
+    print(mask.shape)
     for i, p in enumerate(elem['pos']):
+      if (i==0):
+        print(p)
       #np.append(arr, [[p['x'], p['y']]])
       arr[i,0] = int(round(p['x']))
       arr[i,1] = int(round(p['y']))
 
-    cv2.drawContours(mask, [arr.astype(np.int32)], -1, (255, 255, 255), -1, cv2.LINE_AA)
+    #print(arr.astype(np.int32))
+    print(arr[0])
+    
+
+    cv2.drawContours(mask, [arr.astype(np.int32)], 0, (255, 255, 255), -1, cv2.LINE_AA)
     masks.append(mask)
 
   return np.array(masks)
 
 # Load Data from downloaded JSON
-data = loadData('tags/Products-Poly.json')
+data = loadData('tags/Person-mask.json')
 
 # For all the image get the masks for each label
 for img in data:
   image = cv2.imread('images/' + img['image'],0)
   masks = createMasks(image, img['tags'])
+
+  plt.imshow(image)
+  plt.show()
   
   if (len(masks) == 0):
     continue
