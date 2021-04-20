b0f56e7656afc55757bae4b4e3c51eb72932388a
Diego Parra <diego@linkedai.co>
2021-04-20 12:13:19 -0500

    Create  a Unique script to create the Segmentation, update the script to support multiple cases

--- PolygonToMask.py
+++ PolygonToMask.py
@@ -11,29 +11,33 @@ def loadData(path):
   return data
 
 # with the image and the json get all the mask for each label in the same image
-def createMasks(image, data):
-  masks =  []
+def create_mask(image, data, classes):
+  mask = np.zeros(image.shape[0:2], dtype=np.uint8)
 
   for elem in data:
-    mask = np.zeros(image.shape[0:2], dtype=np.uint8)
+    if elem['type'] != 'polygon':
+            continue
     arr = np.zeros((len(elem['pos']), 2))
 
     for i, p in enumerate(elem['pos']):
       arr[i,0] = int(round(p['x']))
       arr[i,1] = int(round(p['y']))
 
-    cv2.drawContours(mask, [arr.astype(np.int32)], -1, (255, 255, 255), -1, cv2.LINE_AA)
-    masks.append(mask)
+    # Draw the element wit a color depending on the Class
+    cv2.drawContours(mask, [arr.astype(np.int32)], -1, (1 + classes.index(elem['name']), 255, 255), -1, cv2.LINE_AA)
 
-  return np.array(masks)
+  return mask
 
 # Load Data from downloaded JSON
 data = loadData('tags/example-mask.json')
 
+# Define the classes
+classes = ['Person', 'House', 'Cushion', 'Chair', 'Computer']
+
 # For all the image get the masks for each label
 for img in data:
   image = cv2.imread('images/' + img['image'],0)
-  masks = createMasks(image, img['tags'])
+  mask = create_mask(image, img['tags'], classes)
 
   plt.imshow(image)
   plt.show()
